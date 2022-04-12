6c2955a5b7322ba8e55b4c081a35f39d987f5088
Alexander Koskovich <akoskovich@pm.me>
2022-04-11 21:13:13 -0700

    sm7435-common: Sort kernel cmdline and additional flags as a list
     * This allows better readability and eases tracking further changes.
    
    Change-Id: I3a23f7ccb238b5f0e18dfa18080f1ae8ff51109f

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -78,11 +78,12 @@ DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 # Kernel
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE += printk.devkmsg=on
-#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
-BOARD_BOOTCONFIG += androidboot.hardware=qcom
-BOARD_BOOTCONFIG += androidboot.memcg=1
-BOARD_BOOTCONFIG += androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += \
+    printk.devkmsg=on
+BOARD_BOOTCONFIG += \
+    androidboot.hardware=qcom \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
