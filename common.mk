21ef02d0c8963a50c37c3bc940b352ce8de250a4
Arian <arian.kulmer@web.de>
2022-08-28 17:58:43 +0200

    sm7435-common: Copy powerhint.xml from power HAL
    Change-Id: I6f8ca59f4ad0ed7324010f657475d020fa76cb2f
    (cherry picked from commit 42c205f493acf44f66d0600632b351ef8306c81c)

--- common.mk
+++ common.mk
@@ -290,6 +290,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti
 
+PRODUCT_COPY_FILES += \
+    vendor/qcom/opensource/power/config/parrot/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # QMI
 PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti_vendor # Needed by CNE app
