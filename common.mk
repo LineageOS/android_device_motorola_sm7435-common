6ce189ca2602aa7f831f115e8f9e5c3082e10cb7
Bruno Martins <bgcngm@gmail.com>
2025-10-26 09:54:52 +0000

    sm7435-common: Enable eUICC support
    Change-Id: I5471a1d9a0a11cc343fb65f5aff4dcffc4ef52c4

--- common.mk
+++ common.mk
@@ -319,6 +319,7 @@ PRODUCT_PACKAGES += \
     extphonelib-product \
     extphonelib.xml \
     extphonelib_product.xml \
+    MotoEuicc \
     MotoNrEnabler \
     ims-ext-common \
     ims_ext_common.xml \
