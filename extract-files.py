2bb989d74a9416e6f76380487500244d9ee591aa
Nick Reuter <nreuter85@gmail.com>
2025-12-19 22:29:41 -0600

    sm7435-common: Revive qcom wifi display
    Change-Id: Iad1cc73e1b6825b50085404380bb3300f485f128

--- extract-files.py
+++ extract-files.py
@@ -21,6 +21,7 @@ namespace_imports = [
     'hardware/motorola',
     'hardware/qcom-caf/sm8450',
     'hardware/qcom-caf/wlan',
+    'vendor/qcom/opensource/commonsys-intf/display',
     'vendor/qcom/opensource/commonsys/display',
     'vendor/qcom/opensource/dataservices',
 ]
@@ -32,6 +33,7 @@ libs_add_vendor_suffix = (
     'vendor.qti.qspmhal@1.0',
     'vendor.qti.imsrtpservice@3.0',
     'vendor.qti.diaghal@1.0',
+    'vendor.qti.hardware.wifidisplaysession@1.0',
     'com.qualcomm.qti.dpm.api@1.0',
 )
 
@@ -52,6 +54,17 @@ lib_fixups: lib_fixups_user_type = {
 blob_fixups: blob_fixups_user_type = {
     'system_ext/etc/permissions/moto-telephony.xml': blob_fixup().regex_replace(
         '/system/', '/system_ext/'
+    ),
+    'system_ext/lib64/libwfdmmsrc_system.so': blob_fixup().add_needed(
+        'libgui_shim.so'
+    ),
+    'system_ext/lib64/libwfdnative.so': blob_fixup().replace_needed(
+        'android.hidl.base@1.0.so', 'libhidlbase.so'
+    )
+    .add_needed('libbinder_shim.so')
+    .add_needed('libinput_shim.so'),
+    'system_ext/lib64/libwfdservice.so': blob_fixup().replace_needed(
+        'android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'
     ),
     (
         'vendor/bin/hw/android.hardware.security.keymint-service-qti',
