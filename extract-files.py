0b908630a167860a2627002f43984dd15f8ec088
Nick Reuter <nreuter85@gmail.com>
2026-03-30 20:24:11 -0500

    sm7435-common: Reformat blob_fixups
    Change-Id: I49b9d6f6605c642c699cad52ef39d0d1012767b2

--- extract-files.py
+++ extract-files.py
@@ -55,80 +55,42 @@ lib_fixups: lib_fixups_user_type = {
 
 
 blob_fixups: blob_fixups_user_type = {
-    'system_ext/etc/permissions/moto-telephony.xml': blob_fixup().regex_replace(
-        '/system/', '/system_ext/'
-    ),
-    'system_ext/lib64/libwfdnative.so': blob_fixup().add_needed(
-        'libinput_shim.so'
-    ),
-    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup().replace_needed(
-        'libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'
-    ),
-
-    (
-        'vendor/lib64/libgarden.so',
-        'vendor/lib64/libgarden_haltests_e2e.so'
-    ): blob_fixup()
-    .replace_needed(
-        'android.hardware.gnss-V1-ndk_platform.so',
-        'android.hardware.gnss-V1-ndk.so'
-    )
-    .replace_needed(
-        'vendor.qti.gnss-V3-ndk_platform.so',
-        'vendor.qti.gnss-V5-ndk_platform.so'
-    ),
-    (
-        'vendor/bin/hw/android.hardware.security.keymint-service-qti',
-        'vendor/lib64/libqtikeymint.so',
-    ): blob_fixup()
-    .replace_needed(
-        'android.hardware.security.keymint-V1-ndk_platform.so',
-        'android.hardware.security.keymint-V1-ndk.so',
-    )
-    .replace_needed(
-        'android.hardware.security.secureclock-V1-ndk_platform.so',
-        'android.hardware.security.secureclock-V1-ndk.so',
-    )
-    .replace_needed(
-        'android.hardware.security.sharedsecret-V1-ndk_platform.so',
-        'android.hardware.security.sharedsecret-V1-ndk.so',
-    )
-    .add_needed('android.hardware.security.rkp-V1-ndk.so'),
-    'vendor/bin/init.kernel.post_boot.sh': blob_fixup().regex_replace(
-        'ro.boot.using_zram_from_fstab', 'ro.vendor.zram.swapon'
-    ),
-    'vendor/etc/sensors/hals.conf': blob_fixup().add_line_if_missing(
-        'sensors.moto_ext.so',
-    ),
+    'system_ext/etc/permissions/moto-telephony.xml': blob_fixup()
+        .regex_replace('/system/', '/system_ext/'),
+    'system_ext/lib64/libwfdnative.so': blob_fixup()
+        .add_needed('libinput_shim.so'),
+    'system_ext/lib64/vendor.qti.hardware.qccsyshal@1.2-halimpl.so': blob_fixup()
+        .replace_needed('libprotobuf-cpp-full.so', 'libprotobuf-cpp-full-21.7.so'),
+    'system_ext/priv-app/ims/ims.apk': blob_fixup()
+        .apktool_patch('ims-patches'),
+    ('vendor/bin/hw/android.hardware.security.keymint-service-qti','vendor/lib64/libqtikeymint.so',): blob_fixup()
+        .replace_needed('android.hardware.security.keymint-V1-ndk_platform.so', 'android.hardware.security.keymint-V1-ndk.so')
+        .replace_needed('android.hardware.security.secureclock-V1-ndk_platform.so','android.hardware.security.secureclock-V1-ndk.so')
+        .replace_needed('android.hardware.security.sharedsecret-V1-ndk_platform.so', 'android.hardware.security.sharedsecret-V1-ndk.so')
+        .add_needed('android.hardware.security.rkp-V1-ndk.so'),
+    'vendor/bin/init.kernel.post_boot.sh': blob_fixup()
+        .regex_replace('ro.boot.using_zram_from_fstab', 'ro.vendor.zram.swapon'),
+    'vendor/etc/sensors/hals.conf': blob_fixup()
+        .add_line_if_missing('sensors.moto_ext.so'),
+    ('vendor/etc/media_codecs_parrot_v0.xml', 'vendor/etc/media_codecs_parrot_v1.xml',
+        'vendor/etc/media_codecs_parrot_v2.xml', 'vendor/etc/media_codecs_ravelin.xml'): blob_fixup()
+        .regex_replace('.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio|dolby_audio).*\n', ''),
     'vendor/etc/public.libraries.txt': blob_fixup()
         .regex_replace('libqti-perfd-client.so\n', ''),
-    'vendor/lib64/libmotext_inf.so': blob_fixup().remove_needed('libril.so'),
-    'system_ext/priv-app/ims/ims.apk': blob_fixup().apktool_patch(
-        'ims-patches'
-    ),
-    'vendor/lib64/vendor.libdpmframework.so': blob_fixup().add_needed(
-        'libhidlbase_shim.so'
-    ),
-    'vendor/lib64/libqcodec2_core.so': blob_fixup().add_needed(
-        'libcodec2_shim.so'
-    ),
-    'vendor/lib64/sensors.moto.so': blob_fixup().add_needed('libbase_shim.so'),
-    (
-        'vendor/etc/media_codecs_parrot_v0.xml',
-        'vendor/etc/media_codecs_parrot_v1.xml',
-        'vendor/etc/media_codecs_parrot_v2.xml',
-        'vendor/etc/media_codecs_ravelin.xml',
-    ): blob_fixup().regex_replace(
-        '.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio|dolby_audio).*\n',
-        '',
-    ),
-    (
-        'vendor/lib64/libdpps.so',
-        'vendor/lib64/libsnapdragoncolor-manager.so',
-    ): blob_fixup().replace_needed(
-        'libtinyxml2.so', 'libtinyxml2-v34.so'
-    ),
-}
+    ('vendor/lib64/libdpps.so', 'vendor/lib64/libsnapdragoncolor-manager.so'): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+    ('vendor/lib64/libgarden.so', 'vendor/lib64/libgarden_haltests_e2e.so'): blob_fixup()
+        .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so')
+        .replace_needed('vendor.qti.gnss-V3-ndk_platform.so','vendor.qti.gnss-V5-ndk_platform.so'),
+    'vendor/lib64/libmotext_inf.so': blob_fixup()
+        .remove_needed('libril.so'),
+    'vendor/lib64/libqcodec2_core.so': blob_fixup()
+        .add_needed('libcodec2_shim.so'),
+    'vendor/lib64/sensors.moto.so': blob_fixup()
+        .add_needed('libbase_shim.so'),
+    'vendor/lib64/vendor.libdpmframework.so': blob_fixup()
+        .add_needed('libhidlbase_shim.so'),
+} # fmt: skip
 
 module = ExtractUtilsModule(
     'sm7435-common',
