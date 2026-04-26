53a80ee3bce05dc68c51b849b3801320d74171ab
Nikita Savchenko <NikitaSavc@yandex.ru>
2026-04-26 16:02:58 +0000

    sm7435-common: Include qcom common overlays
    Change-Id: Id5f721211a3c391805e3fe0297c047d32e66be48

--- common.mk
+++ common.mk
@@ -200,16 +200,15 @@ PRODUCT_PACKAGES += \
     vendor_super_modem_mountpoint
 
 # Overlays
+$(call inherit-product, hardware/qcom-caf/common/overlay/qssi/qssi.mk)
+
 PRODUCT_PACKAGES += \
     FrameworksResCommon \
     LineageSdkCommon \
     SettingsProviderResCommon \
     SystemUIResCommon \
-    TelephonyResCommon \
-    WifiResCommon \
     FrameworksResTarget \
-    NcmTetheringOverlay \
-    WifiResTarget
+    NcmTetheringOverlay
 
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
