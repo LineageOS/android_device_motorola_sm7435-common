1c78edb362c575576b5404c5995bbd738e651d13
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 10:19:42 +0300

    sm7435-common: Drop UFFD flag
    See: Ibb2c5e9191744f6e15f6bced337ba7d0ed9681ca
    Change-Id: I40424ec785b2f1674a5eefe2150bda5c0a170e61

--- common.mk
+++ common.mk
@@ -165,9 +165,6 @@ PRODUCT_PACKAGES += \
     IPACM_cfg.xml \
     IPACM_Filter_cfg.xml
 
-# Kernel
-PRODUCT_ENABLE_UFFD_GC := true
-
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
